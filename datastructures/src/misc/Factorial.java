944482db062ea1064316b0602ae6fb53db0354dc
André Maciel <andretmaciel@gmail.com>
2022-12-23 18:38:00 -0300

    Adding misc codes - factorial and stack

---
+++ datastructures/src/misc/Factorial.java
@@ -0,0 +1,15 @@
+package misc;
+
+public class Factorial {
+
+    public static int factorial(int n) {
+        if (n == 1)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int myFac = factorial(4);
+        System.out.println(myFac);
+    }
+}
