09aca46406f51f121f3df30653135ed98277eb03
AndreMaciel66 <andretmaciel@gmail.com>
2022-12-24 19:05:16 -0300

    adding sorting algorithms

--- src/algorithms/sorts/bubblesort/BubbleSort.java
+++ src/algorithms/sorts/bubblesort/BubbleSort.java
@@ -1,15 +1,23 @@
 package algorithms.sorts.bubblesort;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void bubbleSort(int[] array) {
-        for (int i = array.length; i > 0; i--) {
+        for (int i = array.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (array[j] > array[j + i]) {
+                if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
             }
         }
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = { 2, 4, 1, 6, 5 };
+        bubbleSort(myArray);
+        System.out.println(Arrays.toString(myArray));
     }
 }
